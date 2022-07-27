fd19b23455caff5f34d6b768cf432ab4595a1dd8
Hunter Hodnett <hchodnett@gmail.com>
2022-07-27 23:28:53 +0000

    fix: build

--- task-overview/src/index.tsx
+++ task-overview/src/index.tsx
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import TaskOverview from "./TaskOverview";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://36ee1b71974e4741be97e5862ac69da1@o1125793.ingest.sentry.io/6553329",
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 declare module "@mui/material/styles" {
   interface Theme {
