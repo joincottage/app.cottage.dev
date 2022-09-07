ccdc75104124d86de060aedc070eafeca09db744
AndreLCanada <andrelacketech@gmail.com>
2022-09-07 12:47:11 +0000

    Add environment variable check to base.ts

--- cottage-api/src/apiService/airtable/base.ts
+++ cottage-api/src/apiService/airtable/base.ts
@@ -1,7 +1,10 @@
 import Airtable from "airtable";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  "appk7ctplKKCsOhWQ"
+  process.end.NEXT_TEST_APP_ENV === 'dev'?
+  "appf3p4te7SoyVHB1" :
+  "appk7ctplKKCsOhWQ" 
+
 );
 
 export default base;
