e4387af249bd5edfe2c5015357c7a42b3b0d00e1
AndreLCanada <andrelacketech@gmail.com>
2022-09-08 00:58:17 +0000

    Remove NEXT_TEST_APP_ from ENV environment variable

--- cottage-api/src/apiService/airtable/base.ts
+++ cottage-api/src/apiService/airtable/base.ts
@@ -1,7 +1,7 @@
 import Airtable from "airtable";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.end.NEXT_TEST_APP_ENV === 'dev'?
+  process.env.ENV === 'dev'?
   "appf3p4te7SoyVHB1" :
   "appk7ctplKKCsOhWQ" 
 
