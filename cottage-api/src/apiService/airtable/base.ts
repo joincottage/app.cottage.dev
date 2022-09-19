70928d037aa776450d24c443b8c92bcc95ec4e23
Li <lixiaomengg@gmail.com>
2022-09-19 23:22:12 +0000

    Add Airtable Integration

--- cottage-api/src/apiService/airtable/base.ts
+++ cottage-api/src/apiService/airtable/base.ts
@@ -1,6 +1,8 @@
 import Airtable from "airtable";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.NEXT_APP_TEST_ENV === "dev" ? 
+  "appf3p4te7SoyVHB1" :
   "appk7ctplKKCsOhWQ"
 );
 
---
+++ cottage-app/src/pages/create-task/types/Task.ts
@@ -0,0 +1,8 @@
+export interface Task {
+  "Record ID"?: string,
+  Name: string,
+  "Acceptance Criteria": string,
+  Description: string,
+  "Figma Direct Link": string | null,
+  "Figma Embed": string | null,
+}
