9287e11a382dfcc69af4c95e2dd64465f9b1881e
Hunter Hodnett <hchodnett@gmail.com>
2022-09-21 22:18:53 +0000

    feat: default project files

--- cottage-app/src/pages/create-task/components/Editor.tsx
+++ cottage-app/src/pages/create-task/components/Editor.tsx
@@ -30,16 +30,74 @@ export default function Editor({ task }: OwnProps) {
                 ? task[0]["Open File Name"]
                 : undefined,
             height: window.innerHeight,
-            forceEmbedLayout: true,
+            forceEmbedLayout: false,
           }
         ));
     }
 
     async function initializeDefaultEditor() {
       const vm = ((window as any).stackblitzVM =
-        await StackblitzSDK.embedProjectId("editor", "react-ts-bwmw61", {
-          height: window.innerHeight,
-        }));
+        await StackblitzSDK.embedProject(
+          "editor",
+          {
+            title: "React Typescript Component",
+            description:
+              "A react-typescript starter project for your component",
+            template: "create-react-app",
+            files: {
+              "src/index.js": `import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);`,
+              "public/index.html": `<div id="root"></div>`,
+              "src/App.jsx": `import * as React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}`,
+              "package.json": `{
+  "name": "react-ts",
+  "version": "0.0.0",
+  "private": true,
+  "dependencies": {
+    "@types/react": "^18.0.8",
+    "@types/react-dom": "^18.0.2",
+    "react": "^18.1.0",
+    "react-dom": "^18.1.0"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test --env=jsdom",
+    "eject": "react-scripts eject"
+  },
+  "devDependencies": {
+    "react-scripts": "latest"
+  }
+}`,
+            },
+          },
+          {
+            openFile: "src/App.jsx",
+            height: window.innerHeight,
+            forceEmbedLayout: false,
+          }
+        ));
     }
 
     if (task && task[0]["Contents"]) {
