1c8a05548cd466641dc27781e450ff59b63bf7df
Hunter Hodnett <hchodnett@gmail.com>
2022-10-18 03:57:43 +0000

    fix: remove dev env

--- cottage-api/src/apiService/airtable/base.ts
+++ cottage-api/src/apiService/airtable/base.ts
@@ -1,9 +1,5 @@
 import Airtable from "airtable";
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.NEXT_APP_TEST_ENV === "dev" ? 
-  "appf3p4te7SoyVHB1" :
-  "appk7ctplKKCsOhWQ"
-);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appk7ctplKKCsOhWQ');
 
 export default base;
